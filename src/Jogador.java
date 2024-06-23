5fbc87ad40e334125a06362745a23188b07471eb
Mariana de Souza <168456934+marianaSsouza@users.noreply.github.com>
2024-06-23 19:54:17 -0300

    Update de decisões na classe Jogador.java

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,12 +1,12 @@
-package src;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Jogador {
 
     public List<Carta> cartas;
-
     public String nome;
 
     public Jogador(String nome) {
@@ -14,16 +14,134 @@ public class Jogador {
         this.nome = nome;
     }
 
-    public Carta jogada(Carta cartaNaMesa){
-        return cartas.get(0);
+    public Carta jogada(Carta cartaNaMesa) {
+        return new JogadaDecisao().decidirJogada(cartaNaMesa);
     }
 
-
-
-    public void exibeCartas(){
+    public void exibeCartas() { 
         cartas.forEach(c -> {
-            System.out.print(c + " ");
+            System.out.print(c + " "); 
         });
-        System.out.println();
+        System.out.println(); 
+    }
+
+    // classe privada com a logica de decisões
+    private class JogadaDecisao {
+
+        // esse metodo é para decidir cada jogada
+        public Carta decidirJogada(Carta cartaNaMesa) {
+            Carta cartaEscolhida = primeiraMao(cartaNaMesa);
+            if (cartaEscolhida == null) {
+                cartaEscolhida = segundaMao(cartaNaMesa);
+            }
+            if (cartaEscolhida == null) {
+                cartaEscolhida = terceiraMao(cartaNaMesa);
+            }
+            return cartaEscolhida;
+        }
+
+        // metodo para verificar a primeira mão de cartas da partida
+        private Carta primeiraMao(Carta cartaNaMesa) {
+            Optional<Carta> manilha = cartas.stream().filter(c -> c.valor > 13).findFirst(); // metodo para a manilha
+            if (manilha.isPresent()) {
+                return manilha.get();
+            }
+            // para ver se vc tem uma carta forte 
+            Optional<Carta> gato = cartas.stream().filter(c -> c.valor > 10000).findFirst();
+            if (gato.isPresent()) {
+                return gato.get();
+            }
+            // stream para ver qual é a maior, menor e a carta do meio
+            Optional<Carta> maiorCarta = cartas.stream().max(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> menorCarta = cartas.stream().min(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> cartaDoMeio = cartas.stream()
+                    .filter(c -> c.valor != maiorCarta.get().valor && c.valor != menorCarta.get().valor)
+                    .findFirst();
+
+            // if para jogar a carta da primeira mão
+            // se a maior carta é maior que o valor da carta da mesa...
+            if (maiorCarta.isPresent() && maiorCarta.get().valor > cartaNaMesa.valor) {
+                return maiorCarta.get(); // joga a maior carta
+
+            // se a carta do meio é maior que o valor da menor carta...
+            } else if (cartaDoMeio.isPresent() && cartaDoMeio.get().valor > menorCarta.get().valor) {
+                return cartaDoMeio.get(); // joga a carta do meio
+
+             // caso a carta do meio não for maior que a carta menor...
+            } else if (cartaDoMeio.isPresent() && cartaDoMeio.get().valor < menorCarta.get().valor) {
+                return menorCarta.get(); // retorna a menor carta
+            }
+            return null; // isso aqui vai mostrar que a carta foi escolhida
+        }
+
+        // metodo para verificar as cartas da segunda mão
+        private Carta segundaMao(Carta cartaNaMesa) {
+            // ver se vc tem uma manilha
+            Optional<Carta> manilha = cartas.stream().filter(c -> c.valor > 13).findFirst();
+            if (manilha.isPresent()) {
+                return manilha.get();
+            }
+            // ver se tem uma carta forte 
+            Optional<Carta> gato = cartas.stream().filter(c -> c.valor > 10000).findFirst();
+            if (gato.isPresent()) {
+                return gato.get();
+            }
+            // stream para ver qual é a maior, menor e a carta do meio
+            Optional<Carta> maiorCarta = cartas.stream().max(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> menorCarta = cartas.stream().min(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> cartaDoMeio = cartas.stream()
+                    .filter(c -> c.valor != maiorCarta.get().valor && c.valor != menorCarta.get().valor)
+                    .findFirst();
+
+            // se a maior carta é maior que o valor da carta da mesa...
+            if (cartaDoMeio.isPresent() && cartaDoMeio.get().valor == cartaNaMesa.valor + 1) {
+                return cartaDoMeio.get(); // joga a maior carta
+
+            // se a carta do meio é maior que o valor da menor carta...
+            } else if (cartaDoMeio.isPresent() && maiorCarta.get().valor > menorCarta.get().valor) {
+                return maiorCarta.get(); // joga a carta do meio
+
+            // caso a carta do meio não for maior que a carta menor...
+            } else if (cartaDoMeio.isPresent() && cartaDoMeio.get().valor < menorCarta.get().valor) {
+                return menorCarta.get(); // retorna a menor carta
+            }
+            return null; // return para mostrar que a carta foi escolhida
+        }
+
+        // metodo para verificar as cartas da terceira mão
+        private Carta terceiraMao(Carta cartaNaMesa) { //// ERRO
+            // ver se tem uma manilha
+            Optional<Carta> manilha = cartas.stream().filter(c -> c.valor > 13).findFirst();
+            if (manilha.isPresent()) {
+                return manilha.get();
+            }
+            /// ver se tem uma carta forte
+            Optional<Carta> gato = cartas.stream().filter(c -> c.valor > 10000).findFirst();
+            if (gato.isPresent()) {
+                return gato.get();
+            }
+            // stream para ver qual é a maior, menor e a carta do meio
+            Optional<Carta> maiorCarta = cartas.stream().max(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> menorCarta = cartas.stream().min(Comparator.comparingInt(c -> c.valor));
+            Optional<Carta> cartaDoMeio = cartas.stream()
+                    .filter(c -> c.valor != maiorCarta.get().valor && c.valor != menorCarta.get().valor)
+                    .findFirst();
+
+            // se a maior carta do meio tem valor igual ao da carta da mesa +1 ...
+            if (cartaDoMeio.isPresent() && cartaDoMeio.get().valor == cartaNaMesa.valor + 1) {
+                return cartaDoMeio.get(); // mostra a carta do meio
+
+            // se a carta maior é maior que o valor da menor carta...
+            } else if (cartaDoMeio.isPresent() && maiorCarta.get().valor > menorCarta.get().valor) {
+                return maiorCarta.get(); // mostra a maior carta
+
+            // caso não seja nada disso....
+            } else {
+                return menorCarta.get(); // mostra a menor carta
+            }
+
+            return null; 
+         
+        }
     }
 }
